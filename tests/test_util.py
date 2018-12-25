5d4162ca12ceaddb324bf54baab1bf58dc8e3547
supakeen <cmdr@supakeen.com>
2018-12-25 22:39:41 +0000

    fix testcase

--- tests/test_util.py
+++ tests/test_util.py
@@ -5,9 +5,9 @@ from unchaind.mapper import siggy as unchaind_mapper_siggy
 
 def test_get_mapper() -> None:
     obj = unchaind_util.get_mapper("siggy")
-    assert isinstance(obj, unchaind_mapper_siggy.Map) is True
+    assert unchaind_mapper_siggy.Map == obj
 
 
 def test_get_transport() -> None:
     obj = unchaind_util.get_transport("siggy")
-    assert isinstance(obj, unchaind_mapper_siggy.Transport) is True
+    assert unchaind_mapper_siggy.Transport == obj
