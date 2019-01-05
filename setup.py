007159bd53f5b19baa3d8c3770243f34ac543ce8
supakeen <cmdr@supakeen.com>
2019-01-05 12:41:14 +0000

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="unchaind",
-    version="0.1.2",
+    version="0.1.3",
     description="Wormhole space kill notification tool for EVE Online.",
     url="https://github.com/supakeen/unchaind",
     author="supakeen",
